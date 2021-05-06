56b54f80f2db3a67b8267dfce7a0a213446687ad
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2021-05-06 21:00:01 +0200

    update scripts

--- pusher.sh
+++ pusher.sh
@@ -2,10 +2,19 @@
 set -e
 set -x
 
-VOI=$1
+VOIS="WIELKOPOLSKIE DOLNOŚLĄSKIE WARMIŃSKO_MAZURSKIE KUJAWSKO_POMORSKIE LUBELSKIE LUBUSKIE MAŁOPOLSKIE PODKARPACKIE OPOLSKIE ŚLĄSKIE PODLASKIE MAZOWIECKIE ŁÓDZKIE ŚWIĘTOKRZYSKIE POMORSKIE ZACHODNIOPOMORSKIE"
+
+USER_ID=$1
 CLONE_DIR=$(mktemp -d)
 OUTPUT=$(pwd)/output
 
+P_VARNAME=P$1
+S_VARNAME=S$1
+C_VARNAME=C$1
+
+P=${!P_VARNAME}
+S=${!S_VARNAME}
+C=${!C_VARNAME}
 
 echo HOST=$HOST
 
@@ -14,24 +23,36 @@ rm -rf $CLONE_DIR/*
 echo "Generating new data"
 mkdir -p $OUTPUT
 gpg -d --batch --yes -z 0 --cipher-algo AES256 -o test.jar --passphrase "$KEY" enc
-java -jar test.jar -p $P1 -s $S1 -c $C1 -t $OUTPUT -v $VOI
-rm test.jar
+
+JAR_DIR=$(pwd)
 
 echo "Cloning destination git repository"
 git clone --single-branch --branch main "https://szcz""epienia:$PPAT@github.com/szc""zepi""enia/szcze""pien""ia.github.io.git" "$CLONE_DIR"
 cd "$CLONE_DIR"
 
-echo "Adding git commit"
 git config user.email "82411728+szcz""epie""nia@users.noreply.github.com"
 git config user.name "szcz""epie""nia"
 
-cp $OUTPUT/*.html .
-git add *.html
-if git status | grep -q "Changes to be committed"
-then
-    git commit --message "remote update"
-    echo "Pushing git commit"
-    git push -u origin HEAD:main || git pull --rebase --prune --tags &&  git push -u origin HEAD:main
-else
-    echo "No changes detected"
-fi
+for VOI in $VOIS; do
+    cd $JAR_DIR
+    rm -rf $OUTPUT
+    java -jar test.jar -p $P -s $S -c $C -t $OUTPUT -v $VOI
+
+    cd "$CLONE_DIR"
+    git pull --rebase --prune --tags
+
+    cp $OUTPUT/*.html .
+    git add -A
+
+    if git status | grep -q "Changes to be committed"
+    then
+        git commit --message "remote update"
+        echo "Pushing git commit"
+        git push -u origin HEAD:main || git pull --rebase --prune --tags &&  git push -u origin HEAD:main || git pull --rebase --prune --tags &&  git push -u origin HEAD:main
+    else
+        echo "No changes detected"
+    fi
+
+done
+
+rm test.jar
--- refresh.sh
+++ refresh.sh
@@ -2,4 +2,16 @@
 set -e
 set -x
 
-curl -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36' -H 'Accept: application/json, text/plain, */*' --compressed -H "X-Csrf-Token: $C1" -H 'Connection: keep-alive' -H 'Referer: https://pacje''nt.erejes''tracja.ezd''rowie.go''v.pl/wizyty' -H "Cookie: patient_sid=$S1" -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'TE: Trailers' 'https://pac''jent.ere''jestracja.ezd''rowie.g''ov.pl/api/settings'
+USER_ID=$1
+CLONE_DIR=$(mktemp -d)
+OUTPUT=$(pwd)/output
+
+P_VARNAME=P$1
+S_VARNAME=S$1
+C_VARNAME=C$1
+
+P=${!P_VARNAME}
+S=${!S_VARNAME}
+C=${!C_VARNAME}
+
+curl -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36' -H 'Accept: application/json, text/plain, */*' --compressed -H "X-Csrf-Token: $C" -H 'Connection: keep-alive' -H 'Referer: https://pacje''nt.erejes''tracja.ezd''rowie.go''v.pl/wizyty' -H "Cookie: patient_sid=$S" -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'TE: Trailers' 'https://pac''jent.ere''jestracja.ezd''rowie.g''ov.pl/api/settings'
